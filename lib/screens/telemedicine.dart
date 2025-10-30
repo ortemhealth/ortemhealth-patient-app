0b619dab8d371b04aab1bfd6ac0a0be60755f4aa
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-30 12:13:38 +0530

    Update telemedicine.dart

--- lib/screens/telemedicine.dart
+++ lib/screens/telemedicine.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TelemedicineScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Video Consult')),
+        body: Center(
+          child: ElevatedButton.icon(
+            icon: Icon(Icons.video_call),
+            label: Text("Join Call"),
+            onPressed: () {
+              // integrate video link logic here
+              // open Twilio/Agora/Zoom widget
+            },
+          ),
+        ));
+  }
+}
 
