cc03f3654b2d91f7dde3cfc6475cf89896818039
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-30 13:31:29 +0530

    Update notification_badge.dart

--- lib/widgets/notification_badge.dart
+++ lib/widgets/notification_badge.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NotificationBadge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Icon(Icons.notifications, size: 28),
+        Positioned(
+          right: 0, top: 2,
+          child: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(8)),
+            child: Text("3", style: TextStyle(color: Colors.white, fontSize: 12)),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
