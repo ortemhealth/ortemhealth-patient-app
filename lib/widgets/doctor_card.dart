e3277e068757580c1e81b7d4361737301d5d8e5d
Praveen Jha <95619152+praveen1743@users.noreply.github.com>
2025-10-30 13:26:32 +0530

    Update doctor_card.dart

--- lib/widgets/doctor_card.dart
+++ lib/widgets/doctor_card.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DoctorCard extends StatelessWidget {
+  final String name, specialty;
+  DoctorCard({this.name, this.specialty});
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(12),
+      child: ListTile(
+        leading: CircleAvatar(child: Icon(Icons.person)),
+        title: Text(name),
+        subtitle: Text(specialty),
+        trailing: Icon(Icons.chevron_right),
+        onTap: () => Navigator.pushNamed(context, '/appointment_booking'),
+      ),
+    );
+  }
+}
 
